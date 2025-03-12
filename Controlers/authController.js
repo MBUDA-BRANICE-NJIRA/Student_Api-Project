89a17a36d61a096a9ced65f87d98327907f5d581
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-12 11:14:49 +0300

    Worked on the section responsible for handling the students on the auth controller when populating the table

--- Controlers/authController.js
+++ Controlers/authController.js
@@ -63,4 +63,82 @@ module.exports = {
             next(error);
         }
     },
+       getAllStudents: async (request, response, next) => {
+            try {
+                const result = await Student.find();
+                response.send(result);
+            } catch (error) {
+                console.log(error.message);
+            }
+        },
+        addStudent: async (req, res, next) => {
+            try {
+                
+                const student = new Student(req.body);
+                const saveStudent = await student.save();
+                res.send(saveStudent);
+            } catch (error) {
+                if (error.isJoi === true) error.status = 422;
+                next(error.message);
+            }
+        },
+        getStudentById: async (request, response, next) => {
+            const id = request.params.id;
+            try {
+                const student = await Student.findById(id);
+                if (!student) {
+                    throw createError(
+                        404,
+                        "Student does not exist, Please try again"
+                    );
+                }
+                response.send(student);
+            } catch (error) {
+                console.log(error.message);
+                if (error instanceof mongoose.castError) {
+                    next(createError(404, "Invalid student Id"));
+                    return;
+                }
+            }
+        },
+    
+        updateStudent: async (request, response, next) => {
+            try {
+                const id = request.params.id;
+                const update = request.body;
+                const options = { new: true };
+                const result = await Student.findByIdAndUpdate(id, update, options);
+                if (!result) {
+                    throw createError(
+                        404,
+                        "Student does not exist, Please try again"
+                    );
+                }
+                response.send(result);
+            } catch (error) {
+                console.log(error.message);
+                if (error instanceof mongoose.castError) {
+                    return next(createError(404, "Invalid student Id"));
+                }
+            }
+        },
+        deleteStudent: async (request, response, next) => {
+            const id = request.params.id;
+            try {
+                const student = await Student.findByIdAndDelete(id);
+                if (!student) {
+                    throw createError(
+                        404,
+                        "Student does not exist, Please try again"
+                    );
+                }
+                response.send(student);
+            } catch (error) {
+                console.log(error.message);
+                if (error instanceof mongoose.castError) {
+                    next(createError(404, "Invalid student Id"));
+                    return;
+                }
+            }
+        },
 };
